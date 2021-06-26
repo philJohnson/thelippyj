b1fc8310169c6c5eacea6cc3561240397202409e
Phillip Johnson <phil@blstr.co>
2021-06-26 20:42:55 +1000

    site setup

---
+++ site/web/app/themes/postlight-headless-wp/inc/menus.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Register main menu.
+ *
+ * @package  Postlight_Headless_WP
+ */
+
+/**
+ * Register navigation menu.
+ *
+ * @return void
+ */
+function register_menus() {
+    register_nav_menu( 'header-menu', __( 'Header Menu', 'postlight-headless-wp' ) );
+    register_nav_menu( 'footer-menu', __( 'Footer Menu', 'postlight-headless-wp' ) );
+}
+add_action( 'after_setup_theme', 'register_menus' );
